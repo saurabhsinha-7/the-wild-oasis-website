0ef907936dd9ac2cc37adac4e4b14fc0a61c7728
saurabhsinha-7 <sin.sau1@gmail.com>
2024-08-05 15:16:17 +0530

    Initial commit

---
+++ app/_components/ReservationList.js
@@ -0,0 +1,32 @@
+"use client";
+
+import { deleteReservation } from "../_lib/actions";
+import ReservationCard from "./ReservationCard";
+import { useOptimistic } from "react";
+
+function ReservationList({ bookings }) {
+  const [optimisticBookins, optimisticDelete] = useOptimistic(
+    bookings,
+    (curBookings, bookingId) => {
+      return curBookings.filter((booking) => booking.id !== bookingId);
+    }
+  );
+
+  async function handleDelete(bookingId) {
+    optimisticDelete(bookingId);
+    await deleteReservation(bookingId);
+  }
+  return (
+    <ul className="space-y-6">
+      {optimisticBookins.map((booking) => (
+        <ReservationCard
+          booking={booking}
+          key={booking.id}
+          onDelete={handleDelete}
+        />
+      ))}
+    </ul>
+  );
+}
+
+export default ReservationList;
---
+++ app/_lib/actions.js
@@ -0,0 +1,116 @@
+"use server";
+
+import { revalidatePath } from "next/cache";
+import { auth, signIn, signOut } from "./auth";
+import { supabase } from "./supabase";
+import { getBookings } from "./data-service";
+import { redirect } from "next/navigation";
+
+export async function updateGuest(formData) {
+  const session = await auth();
+  if (!session) throw new Error("You must be logged in");
+
+  const nationalID = formData.get("nationalID");
+  const [nationality, countryFlag] = formData.get("nationality").split("%");
+
+  if (!/^[a-zA-Z0-9]{6,12}$/.test(nationalID))
+    throw new Error("Please provide a valid national ID");
+
+  const updateData = { nationality, countryFlag, nationalID };
+  const { data, error } = await supabase
+    .from("guest")
+    .update(updateData)
+    .eq("id", session.user.guestId);
+
+  if (error) {
+    console.error(error);
+    throw new Error("Guest could not be updated");
+  }
+
+  revalidatePath("/account/profile");
+}
+
+export async function createBooking(bookingData, formData) {
+  const session = await auth();
+  if (!session) throw new Error("You must be logged in");
+
+  const newBooking = {
+    ...bookingData,
+    guestId: session.user.guestId,
+    numGuests: Number(formData.get("numGuests")),
+    observations: formData.get("observations").slice(0, 1000),
+    extrasPrice: 0,
+    totalPrice: bookingData.cabinPrice,
+    status: "unconfirmed",
+    isPaid: false,
+    hasBreakfast: false,
+  };
+  const { error } = await supabase.from("bookings").insert([newBooking]);
+
+  if (error) {
+    console.error(error);
+    throw new Error("Booking could not be created");
+  }
+
+  revalidatePath(`/cabins/${bookingData.cabinId}`);
+  redirect("/cabins/thankyou");
+}
+
+export async function deleteReservation(bookingId) {
+  const session = await auth();
+  if (!session) throw new Error("You must be logged in");
+
+  const guestBookings = await getBookings(session.user.guestId);
+  const guestBookingIds = guestBookings.map((booking) => booking.id);
+
+  if (!guestBookingIds.includes(bookingId))
+    throw new Error("You are not allowed to delete this booking");
+
+  const { error } = await supabase
+    .from("bookings")
+    .delete()
+    .eq("id", bookingId);
+
+  if (error) {
+    console.error(error);
+    throw new Error("Booking could not be deleted");
+  }
+
+  revalidatePath("/account/reservations");
+}
+
+export async function updateBooking(formData) {
+  const session = await auth();
+  if (!session) throw new Error("You must be logged in");
+
+  const id = Number(formData.get("id"));
+
+  const updatedFields = {
+    numGuests: Number(formData.get("numGuests")),
+    observations: formData.get("observations").slice(0, 1000),
+  };
+
+  const { error } = await supabase
+    .from("bookings")
+    .update(updatedFields)
+    .eq("id", id)
+    .select()
+    .single();
+
+  if (error) {
+    console.error(error);
+    throw new Error("Booking could not be updated");
+  }
+
+  revalidatePath("/account/reservations");
+  revalidatePath(`/account/reservations/edit/${id}`);
+  redirect("/account/reservations");
+}
+
+export async function signInAction() {
+  await signIn("google", { redirectTo: "/account" });
+}
+
+export async function signOutAction() {
+  await signOut({ redirectTo: "/" });
+}
